b3e866b3bb0c68ee6d22ea95addcd4e66052fc12
Marcos Serradilla Diez <marcos@sanlab.io>
2018-01-22 20:47:56 +0100

    Refactorización de las llamadas a líneas de comando.

--- lib/node.go
+++ lib/node.go
@@ -3,7 +3,6 @@ package lib
 import (
 	"bytes"
 	"fmt"
-	"log"
 	//	"math/big"
 	"encoding/hex"
 	"io"
@@ -15,9 +14,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/astaxie/beego/logs"
 	"github.com/hashicorp/go-getter"
 	//"github.com/alastria/monitor/services"
 )
+
+var log *logs.BeeLogger
 
 var homeDir = os.Getenv("HOME")
 
@@ -33,68 +35,60 @@ func Restart() bool {
 	return true
 }
 
-func debugCommand() {
-	cmd := exec.Command(homeDir + "/alastria-node/scripts/stop.sh")
+func runCommand(command string) (ok bool, salida string) {
+	cmd := exec.Command(command)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error: " + fmt.Sprint(err) + ": " + stderr.String())
+		ok = false
+		salida = "Error: " + fmt.Sprint(err) + ": " + stderr.String()
+		log.Debug(salida)
 		return
 	}
-	fmt.Println("Result: " + out.String())
+	salida = "Result: " + out.String()
+	log.Debug(salida)
+	return
 }
 
 // Stop an Alastria node
-func Stop() bool {
-	out, err := exec.Command(homeDir + "/alastria-node/scripts/stop.sh").Output()
-	fmt.Println(out, err)
-	time.Sleep(100 * time.Millisecond)
-	if err != nil {
-		return false
-	}
-	return true
+func Stop() (ok bool) {
+	ok, _ = runCommand(homeDir + "/alastria-node/scripts/stop.sh")
+	return
 }
 
 // Start an Alastria node
-func Start() bool {
-	out, err := exec.Command(homeDir + "/alastria-node/scripts/start.sh").Output()
-	fmt.Println(out, err)
-	time.Sleep(100 * time.Millisecond)
-	if err != nil {
-		return false
-	}
-	return true
+func Start() (ok bool) {
+	ok, _ = runCommand(homeDir + "/alastria-node/scripts/start.sh")
+	return
 }
 
 // Update config files and restart an Alastria node
 func Update() bool {
 	var err error
-	fmt.Printf("%s, %s, %s, %s", IDENTITY, NODE_TYPE, STATIC, PERMISSIONED)
+	log.Debug("%s, %s, %s, %s", IDENTITY, NODE_TYPE, STATIC, PERMISSIONED)
 	stfile, static := getGithub("https://raw.githubusercontent.com/alastria/alastria-node/feature/ibft/data/static-nodes.json")
 	pmfile, permissioned := getGithub("https://raw.githubusercontent.com/alastria/alastria-node/feature/ibft/data/permissioned-nodes_" + NODE_TYPE + ".json")
 	if strings.Compare(static, STATIC) != 0 || strings.Compare(permissioned, PERMISSIONED) != 0 {
 
-		fmt.Println("Son distintos")
+		log.Trace("Son distintos")
 		if strings.Compare(static, STATIC) != 0 {
-			fmt.Println("Actualizando static-nodes")
+			log.Trace("Actualizando static-nodes")
 			copy(stfile, STATIC_NODES)
 		}
 		if strings.Compare(permissioned, PERMISSIONED) != 0 {
-			fmt.Println("Actualizando permissioned-nodes")
+			log.Trace("Actualizando permissioned-nodes")
 			copy(pmfile, PERMISSIONED_NODES)
 		}
-		fmt.Println(strings.Trim(static, "]"), strings.Trim(STATIC, "]"))
+		log.Debug(strings.Trim(static, "]"), strings.Trim(STATIC, "]"))
 		if !strings.Contains(strings.Trim(static, "]"), strings.Trim(STATIC, "]")) ||
 			!strings.Contains(strings.Trim(permissioned, "]"), strings.Trim(PERMISSIONED, "]")) {
-			fmt.Println("Hay que reiniciar el nodo...")
-			out, err := exec.Command(homeDir + "/alastria-node/scripts/stop.sh").Output()
-			fmt.Println(out, err)
+			log.Trace("Hay que reiniciar el nodo...")
+			runCommand(homeDir + "/alastria-node/scripts/stop.sh")
 			time.Sleep(15000 * time.Millisecond)
-			out, err = exec.Command(homeDir+"/alastria-node/scripts/start.sh", "all").Output()
-			fmt.Println(out, err)
+			runCommand(homeDir + "/alastria-node/scripts/start.sh all")
 		}
 	}
 	if err != nil {
@@ -104,16 +98,10 @@ func Update() bool {
 }
 
 // Propose new candidate
-func Propose(candidate string, value string) bool {
+func Propose(candidate string, value string) (ok bool) {
 	cmdStr := "geth --exec 'istanbul.propose(\"" + candidate + "\", " + value + ")' attach http://localhost:22000"
-	out, err := exec.Command(cmdStr).Output()
-	fmt.Println(out, err)
-	time.Sleep(100 * time.Millisecond)
-	//TODO: Check the output of the command. We can test the condition commented below
-	if err != nil { //|| string(out) != "null" {
-		return false
-	}
-	return true
+	ok, _ = runCommand(cmdStr)
+	return
 }
 
 // Non-returning Update function for its use in CRON
@@ -122,22 +110,16 @@ func UpdateCron() {
 }
 
 //Compute Status for a node
-func Status() bool {
-	out, err := exec.Command("ps aux | grep geth  | grep alastria/data | grep -v grep | awk '{print $2}')").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if string(out) != "" {
-		return true
-	}
-	return false
+func Status() (ok bool) {
+	ok, _ = runCommand("ps aux | grep geth  | grep alastria/data | grep -v grep | awk '{print $2}'")
+	return
 }
 
 func getGithub(url string) (filename, contenido string) {
 	filename = tempFileName("monitor", ".json")
 	err := getter.GetFile(filename, url)
 	if err != nil {
-		fmt.Println(err)
+		log.Warn("getGithub: %s", err)
 	}
 	if err == nil {
 		contenido = getFile(filename)
@@ -152,15 +134,15 @@ func getFile(fichero string) (contenido string) {
 }
 
 func copy(stfrom, stto string) {
-	fmt.Println(stfrom, stto)
+	log.Debug(stfrom, stto)
 	from, err := os.Open(stfrom)
 	if err != nil {
-		fmt.Println(err)
+		log.Warn("copy: ", err)
 	}
 	defer from.Close()
 	to, err := os.Open(stto)
 	if err != nil {
-		fmt.Println(err)
+		log.Warn("copy: ", err)
 	}
 	defer to.Close()
 	io.Copy(from, to)
