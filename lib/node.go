1a4c047a0f2665629603fc3dce1d208095bc8655
Alfonso de la Rocha <alfon.rocha@gmail.com>
2018-06-19 10:02:16 +0000

    testing error managemnt in #7

--- lib/node.go
+++ lib/node.go
@@ -134,11 +134,11 @@ func Update() bool {
 		// log.Trace("Son distintos")
 		if strings.Compare(static, STATIC) != 0 {
 			// log.Trace("Actualizando static-nodes")
-			copy(stfile, STATIC_NODES)
+			err := copy(stfile, STATIC_NODES)
 		}
 		if strings.Compare(permissioned, PERMISSIONED) != 0 {
 			// log.Trace("Actualizando permissioned-nodes")
-			copy(pmfile, PERMISSIONED_NODES)
+			err  := copy(pmfile, PERMISSIONED_NODES)
 		}
 		// log.Debug(strings.Trim(static, "]"), strings.Trim(STATIC, "]"))
 		if !strings.Contains(strings.Trim(static, "]"), strings.Trim(STATIC, "]")) ||
@@ -263,9 +263,9 @@ func GetFile(fichero string) (contenido string) {
 }
 
 func copy(stfrom, stto string) (err error) {
-	_, ok := RunCommand("cp " + stfrom + " " + stto)
+	ok, _ := RunCommand("cp " + stfrom + " " + stto)
 	if !ok {
-	    return errors.New("Error copyting files")
+	    return errors.New("Error copying files")
 	}
 }
 
